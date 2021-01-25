117a6105f185a8c1d463601b848df90a4d36a89c
wouterKenis1 <kenis.wouter@gmail.com>
2021-01-25 17:04:03 +0100

    backup van create post met nullable id/user

--- frontend/src/app/models/Post.ts
+++ frontend/src/app/models/Post.ts
@@ -1,13 +1,13 @@
 import {User} from "./User";
 
 export class Post{
-  id: number;
-  user: User;
+  id: number| null;
+  user: User | null;
   postTitle: string;
   postContent: string;
 
 
-  constructor(id: number, user: User, postTitle: string, postContent: string) {
+  constructor(id: number| null, user: User | null, postTitle: string, postContent: string) {
     this.id = id;
     this.user = user;
     this.postTitle = postTitle;
---
+++ frontend/src/app/post-create/post-create.component.ts
@@ -0,0 +1,39 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../models/User";
+import {PostService} from "../services/post.service";
+import {Post} from "../models/Post";
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  @Input() currentUser: User = new User();
+  content: string = "";
+  title: string = "";
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  public createPost() :boolean{
+    let post:Post = new Post(null,null,this.title,this.content);
+
+    this.postService.createPost(post).toPromise().then(post => {
+      console.log(post);
+    });
+
+    // this.postService.createPost(this.currentUser.id,this.title,this.content).toPromise().then(successBool =>{
+    //   return successBool;
+    // });
+
+    return true;
+  }
+
+
+}
